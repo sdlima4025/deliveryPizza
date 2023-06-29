0d956df17d499a3a1a15c2ae68bc323079e49d28
sdlima4025 <sergiolimafg3@gmail.com>
2023-06-29 17:28:40 -0300

    criado modal quantidade ==modalQt , não implementado.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let modalQt = 1;
+
 // PROTOTIPO DE QUERY'S
 const qs = (el)=>document.querySelector(el);
 const qsa = (el)=>document.querySelectorAll(el);
@@ -6,6 +8,8 @@ const qsa = (el)=>document.querySelectorAll(el);
 pizzaJson.map((item, index)=>{
     let pizzaItem = qs('.models .pizza-item').cloneNode(true);
 
+
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
@@ -13,9 +17,29 @@ pizzaJson.map((item, index)=>{
 
     pizzaItem.querySelector('a').addEventListener('click', (e)=> {
         e.preventDefault();
+        // Selecionando a pizza clicada 
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+            // console.log(pizzaJson[key]);
+            modalQt = 1;
+
+            qs('.pizzaBig img').src = pizzaJson[key].img;
+            qs('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+            qs('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+            qs('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+            qs('.pizzaInfo--size.selected').classList.remove('selected');
+            qsa('.pizzaInfo--size').forEach((size, sizeIndex)=> {
+                if(sizeIndex == 2) {
+                    size.classList.add('selected');
+                }
+
+                size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+
+            });
+
+            qs('.pizzaInfo--qt').innerHTML = modalQt;
 
 
-
+        // MODAL
         qs('.pizzaWindowArea').style.opacity = 0;
         qs('.pizzaWindowArea').style.display = 'flex';
         setTimeout(()=> {
