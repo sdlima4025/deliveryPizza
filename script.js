3aa9880e1c71a42b4cb2932b02e5bc1c89131c2b
sdlima4025 <sergiolimafg3@gmail.com>
2023-07-03 17:19:21 -0300

    adicionando itens ao cart parte 1

--- script.js
+++ script.js
@@ -93,11 +93,23 @@ qsa('.pizzaInfo--size').forEach((size, sizeIndex)=> {
 qs('.pizzaInfo--addButton').addEventListener('click', () => {
     let size = parseInt(qs('.pizzaInfo--size.selected').getAttribute('data-key'));
    
-    cart.push({
-       id:pizzaJson[modalKey].id,
-       size,
-       qt:modalQt
-    });
+    let identifier = pizzaJson[modalKey].id+'#'+size;
+
+    let key = cart.findIndex((item)=> item.identifier === identifier);    
+    
+
+    if(key > -1) {
+        cart[key].qt += modalQt;
+
+    }else {
+           cart.push({
+           identifier,
+           id:pizzaJson[modalKey].id,
+           size,
+           qt:modalQt
+
+        });
+    }
 
     closeModal();
 });
