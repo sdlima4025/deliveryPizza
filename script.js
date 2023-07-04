981ca7311e9f5f69ca8f9ea618a405d435258fe9
sdlima4025 <sergiolimafg3@gmail.com>
2023-07-04 20:37:55 -0300

    criado funcionalidade do cart com tamanho e img da pizza. funcionalidade de add e ou diminuir itens direto no botão + _ do cart.

--- script.js
+++ script.js
@@ -3,114 +3,163 @@ let modalQt = 1;
 let modalKey = 0;
 
 // PROTOTIPO DE QUERY'S
-const qs = (el)=>document.querySelector(el);
-const qsa = (el)=>document.querySelectorAll(el);
+const qs = (el) => document.querySelector(el);
+const qsa = (el) => document.querySelectorAll(el);
 
 // LISTAGEM DAS PIZZAS
-pizzaJson.map((item, index)=>{
-    let pizzaItem = qs('.models .pizza-item').cloneNode(true);
+pizzaJson.map((item, index) => {
+  let pizzaItem = qs(".models .pizza-item").cloneNode(true);
 
+  pizzaItem.setAttribute("data-key", index);
+  pizzaItem.querySelector(".pizza-item--img img").src = item.img;
+  pizzaItem.querySelector(
+    ".pizza-item--price"
+  ).innerHTML = `R$ ${item.price.toFixed(2)}`;
+  pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
+  pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
 
-    pizzaItem.setAttribute('data-key', index);
-    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
-    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
-    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+  // EVENTO DE CLICK
+  pizzaItem.querySelector("a").addEventListener("click", (e) => {
+    e.preventDefault();
+    let key = e.target.closest(".pizza-item").getAttribute("data-key");
+    modalQt = 1;
+    modalKey = key;
 
-    // EVENTO DE CLICK
-    pizzaItem.querySelector('a').addEventListener('click', (e)=> {
-        e.preventDefault();
-        let key = e.target.closest('.pizza-item').getAttribute('data-key');
-            modalQt = 1;
-            modalKey = key;
+    qs(".pizzaBig img").src = pizzaJson[key].img;
+    qs(".pizzaInfo h1").innerHTML = pizzaJson[key].name;
+    qs(".pizzaInfo--desc").innerHTML = pizzaJson[key].description;
+    qs(".pizzaInfo--actualPrice").innerHTML = `R$ ${pizzaJson[
+      key
+    ].price.toFixed(2)}`;
+    qs(".pizzaInfo--size.selected").classList.remove("selected");
+    qsa(".pizzaInfo--size").forEach((size, sizeIndex) => {
+      if (sizeIndex == 2) {
+        size.classList.add("selected");
+      }
 
-            qs('.pizzaBig img').src = pizzaJson[key].img;
-            qs('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
-            qs('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
-            qs('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
-            qs('.pizzaInfo--size.selected').classList.remove('selected');
-            qsa('.pizzaInfo--size').forEach((size, sizeIndex)=> {
-                if(sizeIndex == 2) {
-                    size.classList.add('selected');
-                }
-
-                size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
-
-            });
-
-            qs('.pizzaInfo--qt').innerHTML = modalQt;
-
-
-        // MODAL
-        qs('.pizzaWindowArea').style.opacity = 0;
-        qs('.pizzaWindowArea').style.display = 'flex';
-        setTimeout(()=> {
-            qs('.pizzaWindowArea').style.opacity = 1;
-        }, 200);
-
-        
+      size.querySelector("span").innerHTML = pizzaJson[key].sizes[sizeIndex];
     });
 
-    qs('.pizza-area').append(pizzaItem);
+    qs(".pizzaInfo--qt").innerHTML = modalQt;
+
+    // MODAL
+    qs(".pizzaWindowArea").style.opacity = 0;
+    qs(".pizzaWindowArea").style.display = "flex";
+    setTimeout(() => {
+      qs(".pizzaWindowArea").style.opacity = 1;
+    }, 200);
+  });
+
+  qs(".pizza-area").append(pizzaItem);
 });
 
 // EVENTOS DO MODAL
 function closeModal() {
-    qs('.pizzaWindowArea').style.opacity = 0;
-    setTimeout(() => {
-        qs('.pizzaWindowArea').style.display = 'none';
-    }, 500);
+  qs(".pizzaWindowArea").style.opacity = 0;
+  setTimeout(() => {
+    qs(".pizzaWindowArea").style.display = "none";
+  }, 500);
 }
-qsa('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=> {
-    item.addEventListener('click', closeModal);
-
-});
+qsa(".pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton").forEach(
+  (item) => {
+    item.addEventListener("click", closeModal);
+  }
+);
 
 // add e diminuir do botão modal
-qs('.pizzaInfo--qtmenos').addEventListener('click', ()=> {
-    if(modalQt > 1) {
-        modalQt--;
-        qs('.pizzaInfo--qt').innerHTML = modalQt;
-
-    }
+qs(".pizzaInfo--qtmenos").addEventListener("click", () => {
+  if (modalQt > 1) {
+    modalQt--;
+    qs(".pizzaInfo--qt").innerHTML = modalQt;
+  }
 });
 
-qs('.pizzaInfo--qtmais').addEventListener('click', ()=> {
-    modalQt++;
-    qs('.pizzaInfo--qt').innerHTML = modalQt;
-
+qs(".pizzaInfo--qtmais").addEventListener("click", () => {
+  modalQt++;
+  qs(".pizzaInfo--qt").innerHTML = modalQt;
 });
 
 // seleção de tamanho de pizza
-qsa('.pizzaInfo--size').forEach((size, sizeIndex)=> {
-    size.addEventListener('click', (e) => {
-        qs('.pizzaInfo--size.selected').classList.remove('selected');
-           size.classList.add('selected');
+qsa(".pizzaInfo--size").forEach((size, sizeIndex) => {
+  size.addEventListener("click", (e) => {
+    qs(".pizzaInfo--size.selected").classList.remove("selected");
+    size.classList.add("selected");
+  });
+});
+
+qs(".pizzaInfo--addButton").addEventListener("click", () => {
+  let size = parseInt(qs(".pizzaInfo--size.selected").getAttribute("data-key"));
+  let identifier = pizzaJson[modalKey].id + "#" + size;
+  let key = cart.findIndex((item) => item.identifier === identifier);
+  if (key > -1) {
+    cart[key].qt += modalQt;
+  } else {
+    cart.push({
+      identifier,
+      id: pizzaJson[modalKey].id,
+      size,
+      qt: modalQt,
     });
-
+  }
+  updateCart();
+  closeModal();
 });
 
-qs('.pizzaInfo--addButton').addEventListener('click', () => {
-    let size = parseInt(qs('.pizzaInfo--size.selected').getAttribute('data-key'));
-   
-    let identifier = pizzaJson[modalKey].id+'#'+size;
+// UPDATE CART
 
-    let key = cart.findIndex((item)=> item.identifier === identifier);    
-    
+function updateCart() {
+  if (cart.length > 0) {
+    qs("aside").classList.add("show");
+    qs(".cart").innerHTML = "";
 
-    if(key > -1) {
-        cart[key].qt += modalQt;
+    for (let i in cart) {
+      let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+      let cartItem = qs(".models .cart--item").cloneNode(true);
 
-    }else {
-           cart.push({
-           identifier,
-           id:pizzaJson[modalKey].id,
-           size,
-           qt:modalQt
+      let pizzaSizeName;
+      switch (cart[i].size) {
+        case 0:
+          pizzaSizeName = "P";
+          break;
 
+        case 1:
+          pizzaSizeName = "M";
+          break;
+
+        case 2:
+          pizzaSizeName = "G";
+          break;
+
+        //     case 3:
+        //         pizzaSizeName = 'GG';
+        //         break;
+      }
+
+      let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
+      cartItem.querySelector("img").src = pizzaItem.img;
+      cartItem.querySelector(".cart--item-nome").innerHTML = pizzaName;
+      cartItem.querySelector(".cart--item--qt").innerHTML = cart[i].qt;
+
+      // add e diminuir do botão cart item
+      cartItem.querySelector(".cart--item-qtmenos").addEventListener("click", () => {
+          if (cart[i].qt > 1) {
+            cart[i].qt--;
+          } else {
+            cart.splice(i, 1);
+          }
+          updateCart();
         });
+
+      cartItem.querySelector(".cart--item-qtmais").addEventListener("click", () => {
+          cart[i].qt++;
+          updateCart();
+        });
+
+      qs(".cart").append(cartItem);
     }
-
-    closeModal();
-});
-
+    
+  } else {
+    qs("aside").classList.remove("show");
+  }
+}
